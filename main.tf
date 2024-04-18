e48f1f02b929ec9560a753968b684f04d1bd769a
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 21:44:57 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_docdb_cluster_parameter_group" "main" {
-  family      = "docdb4.0"
+  family      = var.engine_family
   name        = "${local.name_prefix}-pg"
   description = "${local.name_prefix}-pg"
   tags = merge(local.tags, {Name = "${local.name_prefix}-pg"})
@@ -52,3 +52,9 @@ resource "aws_docdb_cluster" "main" {
   engine_version = var.engine_version
 }
 
+resource "aws_docdb_cluster_instance" "main" {
+  count              = var.instance_count
+  identifier         = "${local.name_prefix}-cluster_instance-${count.index+1}"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
